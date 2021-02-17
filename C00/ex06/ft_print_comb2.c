43405c9d02e7bd0f82703d6444ef7129ff078217
jjocher <jjocher@student.42heilbronn.de>
2021-02-17 12:53:09 +0000

    u

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,35 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jjocher <jjocher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/13 08:42:25 by jjocher           #+#    #+#             */
-/*   Updated: 2021/02/15 12:18:56 by jjocher          ###   ########.fr       */
+/*   Created: 2021/02/13 21:41:40 by jjocher           #+#    #+#             */
+/*   Updated: 2021/02/13 22:53:05 by jjocher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	ft_swap(int *a, int *b)
+void	ft_print_comb2(char a, char b, char c, char d)
 {
-	int i;
-
-	i = *a;
-	*a = *b;
-	*b = i;
+	a = '0';
+	b = '0';
+	c = '0';
+	d = '0';
+	while (a <= '9')
+	{
+		while(b <= '9')
+		{
+			write(1, &a, 1);
+			write(1, &b, 1);
+			write(1, "_", 1);
+			while(c <= '9')
+			{
+				write(1, &c, 1);
+				write(1, &d, 1);
+				write(1, ",", 1);
+			}	c++;
+		}	b++;
+		a++;
+	}
 }
 
 int		main(void)
 {
-	int a;
-	int b;
-
-	a = 5;
-	b = 2;
-	printf("%i; %i \n", a, b);
-	ft_swap(&a, &b);
-	printf("%i; %i", a, b);
+	ft_print_comb2(0, 0, 0, 0);
+	return(0);
 }
